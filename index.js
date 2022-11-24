a7c4916e353704d34d297cc14471530eab0c1609
rahi-rahitya <rahityap7@gmail.com>
2022-11-24 17:09:19 +0530

    about postmethod

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=(2000);
+ app.use(bodyParser.urlencoded({
+ 	extended:true
+ }))
+
+ app.use(bodyParser.json())
+ app.use(cors())
+app.get("/",(req,res)=>{
+	res.send("dummy route")
+})
+app.post("/newData",(req,res)=>{
+    //const name=req.body.name,age=req.body.age
+    const {name,password}=req.body;
+	console.log(name,password)
+    res.send("added succesful")
+})
+app.listen(port,()=>console.log("server is running on port 2000"))
